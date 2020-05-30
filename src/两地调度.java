d047fa903ee66904f81eee6be78084e709517082
陈子鹏 <xk18407020430@163.com>
2020-05-30 21:34:22 +0800

    两地调度.java

---
+++ src/两地调度.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class 两地调度 {
+    public int twoCitySchedCost(int[][] costs) {
+        int sum=0;
+        int arr[]=new int[costs.length];
+        for (int i=0;i<costs.length;i++){
+            sum=sum+costs[i][1];
+            arr[i]=costs[i][1]-costs[i][0];
+        }
+        Arrays.sort(arr);
+        for (int i=0;i<arr.length/2;i++){
+            sum=sum-arr[arr.length-i-1];
+        }
+        return sum;
+    }
+}
