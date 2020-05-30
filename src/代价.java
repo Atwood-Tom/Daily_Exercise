db952c024ec9c8afb9828ed50752c68dda25b6d7
陈子鹏 <xk18407020430@163.com>
2020-05-30 22:12:43 +0800

    代价.java

---
+++ src/代价.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 代价 {
+    //链接:https://www.nowcoder.com/questionTerminal/b7985769dc434d85a16717908669bcab
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String x[]=sc.nextLine().split(" ");
+        int arr[]=new int[x.length];
+        for (int i=0;i<arr.length;i++){
+            arr[i]=Integer.parseInt(x[i]);
+        }
+        Arrays.sort(arr);
+        int max=arr[arr.length-1]-arr[0];
+        System.out.println(max);
+    }
+}
