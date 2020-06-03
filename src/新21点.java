b1bdad8f753c740995ee9eda056bc13794372992
Atwood-Tom <xk18407020430@163.com>
2020-06-03 18:11:21 +0800

    新21点.java

---
+++ src/新21点.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 新21点 {
+    public static double new21Game(int N, int K, int W) {
+        if (K==0){
+            return 1.0;
+        }
+        double arr[]=new double[W+K];
+        for (int i=K;i<arr.length;i++){
+            arr[i]=i<=N?1:0;
+        }
+        for (int j=1;j<=W;j++){
+            arr[K-1]+=arr[j+K-1]/W;
+        }
+        for (int j=K-2;j>=0;j--){
+            arr[j]=(arr[j+1]*W+arr[j+1]-arr[j+1+W])/W;
+        }
+        return arr[0];
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a,b,c;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        System.out.println(new21Game(a,b,c));
+    }
+}
