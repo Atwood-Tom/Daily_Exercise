0d23466847aa3267d6d31b89405ac776f623f761
Atwood-Tom <xk18407020430@163.com>
2020-06-13 21:02:28 +0800

    三步问题.java

---
+++ src/三步问题.java
@@ -0,0 +1,17 @@
+public class 三步问题 {
+    //链接：https://leetcode-cn.com/problems/three-steps-problem-lcci/
+    public int waysToStep(int n) {
+        int arr[]=new int[1000000];
+        arr[0]=1;
+        arr[1]=2;
+        arr[2]=4;
+        arr[3]=7;
+        if (n<=4){
+            return arr[n-1]%1000000007;
+        }
+        for (int i=4;i<n;i++){
+            arr[i]=(arr[i-1]+(arr[i-2]+arr[i-3])%1000000007)%1000000007;
+        }
+        return arr[n-1];
+    }
+}
