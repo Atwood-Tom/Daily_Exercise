be1826f97a37e9ea82fbcda7f6eeb72e50e8e8a6
Atwood-Tom <xk18407020430@163.com>
2020-06-13 20:38:56 +0800

    区域和检索.java

---
+++ src/区域和检索.java
@@ -0,0 +1,15 @@
+public class 区域和检索 {
+    //链接：https://leetcode-cn.com/problems/range-sum-query-immutable/
+    int arr[];
+    public void NumArray(int[] nums) {
+        arr=nums;
+    }
+
+    public int sumRange(int i, int j) {
+        int sum=0;
+        for (;i<=j;i++){
+            sum+=arr[i];
+        }
+        return sum;
+    }
+}
