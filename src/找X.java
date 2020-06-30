8688821709eb7e232f5d2f645127d128165dadca
Atwood-Tom <xk18407020430@163.com>
2020-06-30 22:11:03 +0800

    找X.java

---
+++ src/找X.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class 找X {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for (int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int x=sc.nextInt();
+        int i=arr.length-1;
+        for (;i>=0&&x!=arr[i];i--);
+        System.out.println(i);
+    }
+}
