bd1e7c78043b6ed94636bf922b0ac6f33d5f8e7b
Atwood-Tom <xk18407020430@163.com>
2020-07-23 10:42:07 +0800

    单链表的逆转.java

--- src/单链表的逆转.java
+++ src/单链表的逆转.java
@@ -8,16 +8,30 @@ public class 单链表的逆转 {
     }
     public ListNode ReverseList(ListNode head) {
         ListNode newhead=null;
-        ListNode pre=null;
+        ListNode left=null;
         ListNode mid=head;
         while(mid!=null){
-            ListNode last=mid.next;
-            if (last==null){
+            ListNode right=mid.next;
+            if (right==null){
                 newhead=mid;
             }
-            mid.next=pre;
-            pre=mid;
-            mid=last;
+            mid.next=left;
+            left=mid;
+            mid=right;
+        }
+        return newhead;
+    }
+    public ListNode ReverseList1(ListNode head){
+        if (head==null||head.next==null){
+            return head;
+        }
+        ListNode newhead=new ListNode(head.val);
+        ListNode left=head.next;
+        for (;left!=null;){
+            ListNode right=left.next;
+            left.next=newhead;
+            newhead=left;
+            left=right;
         }
         return newhead;
     }
