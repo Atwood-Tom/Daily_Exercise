d5c76a68b1a28e0f0fcda4026a2d577dbdc60d8a
Atwood-Tom <xk18407020430@163.com>
2020-09-13 15:55:01 +0800

    连续子数组的最大和.java

---
+++ src/连续子数组的最大和.java
@@ -0,0 +1,21 @@
+public class 连续子数组的最大和 {
+    //链接:https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof/
+    public static int maxSubArray(int[] nums) {
+        int max=Integer.MIN_VALUE;
+        int sum=0;
+        int i = 0;
+        for (; i < nums.length; i++) {
+            sum+=nums[i];
+            max=Math.max(sum,max);
+            if (sum<0){
+                sum=0;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int arr[]=new int[]{-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(arr));
+    }
+}
