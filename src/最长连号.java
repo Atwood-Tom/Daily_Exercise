bac7fecc20f1ec26fc9b83611a01df60a825697c
Atwood-Tom <xk18407020430@163.com>
2020-09-20 13:47:38 +0800

    最长连号.java

---
+++ src/最长连号.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 最长连号 {
+    //链接:https://nanti.jisuanke.com/t/T1606
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if (n==0){
+            System.out.println(0);
+        }
+        int max=0;
+        int helper=1;
+        int arr[]=new int[10000];
+        for (int i=0;i<n;arr[i]=sc.nextInt(),i++);
+        for (int i=0;i<n;i++){
+            if (arr[i]+1==arr[i+1]){
+                helper++;
+            }else {
+                max=Math.max(max,helper);
+                helper=1;
+            }
+        }
+        System.out.println(max);
+    }
+}
