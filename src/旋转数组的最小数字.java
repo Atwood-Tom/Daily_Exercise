35a56220c7279b4ad294c24566d96d2fee9b4e20
Atwood-Tom <xk18407020430@163.com>
2020-09-21 16:43:29 +0800

    旋转数组

--- src/旋转数组的最小数字.java
+++ src/旋转数组的最小数字.java
@@ -1,28 +1,28 @@
 public class 旋转数组的最小数字 {
     //链接:https://www.nowcoder.com/questionTerminal/9f3231a991af4f55b95579b44b7a01ba
-    public int minNumberInRotateArray(int [] array) {
-        if (array.length==0){
+    public int minNumberInRotateArray(int [] numbers) {
+        if (numbers.length==0){
             return 0;
         }
         int left=0;
-        int right=array.length-1;
+        int right=numbers.length-1;
         int mid=0;
         while(left<right){
             if (right-left==1){
-                return array[right];
+                return numbers[right];
             }
             mid=(left+right)/2;
-            if (array[mid]==array[left]){
+            if (numbers[mid]==numbers[left]){
                 int i=left;
-                for (;array[i]<=array[i+1];i++);
-                return array[i+1];
+                for (;numbers[i]<=numbers[i+1];i++);
+                return numbers[i+1];
             }
-            if (array[mid]>array[left]) {
+            if (numbers[mid]>numbers[left]) {
                 left=mid;
             }else {
                 right=mid;
             }
         }
-        return array[right];
+        return numbers[right];
     }
 }
