01bafbaa5e8f88ab141c325fde6801e4d0afb75e
Atwood-Tom <xk18407020430@163.com>
2020-09-22 11:06:18 +0800

    左旋转字符串.java

---
+++ src/左旋转字符串.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class 左旋转字符串 {
+    //链接:https://leetcode-cn.com/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof/
+    public static String reverseLeftWords(String s, int n) {
+        String result=s.substring(n,s.length())+s.substring(0,n);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println(reverseLeftWords(sc.nextLine(),sc.nextInt()));
+    }
+}
