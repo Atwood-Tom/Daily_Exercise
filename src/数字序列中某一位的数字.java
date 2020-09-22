5cf5933594c668e439d1ecda833a955737750b06
Atwood-Tom <xk18407020430@163.com>
2020-09-22 11:07:30 +0800

    数字序列中某一位的数字.java

---
+++ src/数字序列中某一位的数字.java
@@ -0,0 +1,12 @@
+public class 数字序列中某一位的数字 {
+    //链接:https://leetcode-cn.com/problems/shu-zi-xu-lie-zhong-mou-yi-wei-de-shu-zi-lcof/
+    public static int findNthDigit(int n) {
+        if (n<=9){
+            return n;
+        }
+
+    }
+    public static void main(String[] args) {
+        System.out.println(findNthDigit(11));
+    }
+}
