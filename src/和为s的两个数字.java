18e49bf7454de2168576edc9930ed34a7e299ed4
Atwood-Tom <xk18407020430@163.com>
2020-09-23 18:24:07 +0800

    和为s的两个数字.java

---
+++ src/和为s的两个数字.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class 和为s的两个数字 {
+    //链接:https://leetcode-cn.com/problems/he-wei-sde-liang-ge-shu-zi-lcof/
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map=new HashMap<>();
+        int result[]=new int[2];
+        for (int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        for (int i=0;i<nums.length;i++){
+            int mid=target-nums[i];
+            if (!map.containsKey(mid)){
+                continue;
+            }
+            int x=map.get(mid);
+            if (mid==nums[i]||x<2){
+                continue;
+            }else if (x>=1){
+                result[0]=mid;
+                result[1]=nums[i];
+            }
+        }
+        return result;
+    }
+    public int[] twoSum1(int[] nums, int target){
+        int i=0;
+        int j=nums.length-1;
+        while(i<j){
+            int sum=nums[i]+nums[j];
+            if (sum>target) j--;
+            else if (sum<target) i++;
+            else return new int[]{nums[i],nums[j]};
+        }
+        return new int[2];
+    }
+}
