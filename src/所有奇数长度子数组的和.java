20e492b811eba0dcc5320455b1de8bd0fb82a319
Atwood-Tom <xk18407020430@163.com>
2020-10-09 19:24:14 +0800

    所有奇数长度子数组的和.java

---
+++ src/所有奇数长度子数组的和.java
@@ -0,0 +1,14 @@
+public class 所有奇数长度子数组的和 {
+    //链接:https://leetcode-cn.com/problems/sum-of-all-odd-length-subarrays/
+    public int sumOddLengthSubarrays(int[] arr) {
+        int sum=0;
+        for(int i = 1;i <= arr.length;i += 2){
+            for(int j = 0;j + i <= arr.length;j++){
+                for(int k = j;k < j + i;k++){
+                    sum += arr[k];
+                }
+            }
+        }
+        return sum;
+    }
+}
