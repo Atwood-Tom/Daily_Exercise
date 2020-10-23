2150ee66206b54a84dd703e550a64f7009bb722b
Atwood-Tom <xk18407020430@163.com>
2020-10-23 11:03:24 +0800

    长按输入.java

---
+++ src/长按输入.java
@@ -0,0 +1,34 @@
+public class 长按输入 {
+    //链接:https://leetcode-cn.com/problems/long-pressed-name/
+    public static boolean isLongPressedName(String name, String typed) {
+        int i=0;int j=0;
+        for (;i<name.length()&&j<typed.length();j++){
+            if (name.charAt(i)==typed.charAt(j)){
+                i++;
+                continue;
+            }else if (i-1>=0&&name.charAt(i-1)==typed.charAt(j)){
+                continue;
+            }else {
+                return false;
+            }
+        }
+        if (j<typed.length()){
+            for (;j<typed.length();j++){
+                if (name.charAt(i-1)!=typed.charAt(j)){
+                    return false;
+                }
+            }
+        }
+        if(i<name.length()){
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String name="";
+        String typed="";
+        name = "pyplrz"; typed = "ppyypllr";
+        System.out.println(isLongPressedName(name,typed));
+    }
+}
