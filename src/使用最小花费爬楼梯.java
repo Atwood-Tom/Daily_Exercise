73c065c4bca2ffe684948f1cfa48d5f0da119fd1
Atwood-Tom <xk18407020430@163.com>
2020-10-24 22:23:50 +0800

    使用最小花费爬楼梯.java

---
+++ src/使用最小花费爬楼梯.java
@@ -0,0 +1,18 @@
+public class 使用最小花费爬楼梯 {
+    //链接:https://leetcode-cn.com/problems/min-cost-climbing-stairs/
+    public static int minCostClimbingStairs(int[] cost) {
+        if (cost.length==1){
+            return cost[0];
+        }
+        if (cost.length==2){
+            return Math.min(cost[0],cost[1]);
+        }
+        int arr[]=new int[cost.length];
+        arr[0]=0;
+        arr[1]=Math.min(cost[0],cost[1]);
+        for (int i=2;i<cost.length;i++){
+            arr[i]=Math.min(cost[i]+arr[i-1],cost[i-1]+arr[i-2]);
+        }
+        return arr[arr.length-1];
+    }
+}
