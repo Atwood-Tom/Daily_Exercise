cdcae9fb0bc23ce123c56c8aa84da07a43027918
Atwood-Tom <xk18407020430@163.com>
2020-11-03 22:50:59 +0800

    比特位计数.java

---
+++ src/比特位计数.java
@@ -0,0 +1,19 @@
+public class 比特位计数 {
+    //链接:https://leetcode-cn.com/problems/counting-bits/
+    public int[] countBits(int num) {
+        int result[]=new int[num+1];
+        for (int i=0;i<result.length;i++){
+            result[i]=time(i);
+        }
+        return result;
+    }
+
+    private int time(int i) {
+        int sum=0;
+        while(i!=0){
+            i=i&(i-1);
+            sum++;
+        }
+        return sum;
+    }
+}
