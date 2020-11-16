60a45bcb4f6cbf494234c5e08205587e1f872b50
Atwood-Tom <xk18407020430@163.com>
2020-11-16 23:08:09 +0800

    根据身高重建队列.java

---
+++ src/根据身高重建队列.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class 根据身高重建队列 {
+    //链接:https://leetcode-cn.com/problems/queue-reconstruction-by-height/submissions/
+    public static int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, new Comparator<int[]>() {
+            public int compare(int[] person1, int[] person2) {
+                if (person1[0] != person2[0]) {
+                    return person2[0] - person1[0];
+                } else {
+                    return person1[1] - person2[1];
+                }
+            }
+        });
+        List<int[]> ans = new ArrayList<int[]>();
+        for (int[] person : people) {
+            ans.add(person[1], person);
+        }
+        return ans.toArray(new int[ans.size()][]);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int arr[][]=new int[x][2];
+        for(int i=0;i<x;i++){
+            arr[i][0]=sc.nextInt();
+            arr[i][1]=sc.nextInt();
+        }
+        reconstructQueue(arr);
+    }
+}
