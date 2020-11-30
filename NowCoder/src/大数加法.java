d6e6e3be4fdd3bf358d0816de76363e92353c1ba
Atwood-Tom <xk18407020430@163.com>
2020-11-30 22:32:03 +0800

    大数加法.java

---
+++ NowCoder/src/大数加法.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class 大数加法 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNextLine()){
+            String x=sc.nextLine();
+            String y=sc.nextLine();
+            System.out.println(Solution(x, y));
+        }
+    }
+
+    private static String Solution(String x, String y) {
+        String sb=new String();
+        int up=0;
+        int i=x.length()-1,j=y.length()-1;
+        for (;i>=0&&j>=0;i--,j--){
+            int a=x.charAt(i)-'0';
+            int b=y.charAt(j)-'0';
+            int add=(a+b+up)%10;
+            sb=add+sb;
+            up=(a+b+up)/10;
+        }
+        if (i==0){
+            for (;j>=0;j--){
+                int a=y.charAt(j)-'0';
+                int add=(a+up)%10;
+                up=(a+up)/10;
+                sb=add+sb;
+            }
+        } else if (j==0){
+            for (;i>=0;i--){
+                int a=y.charAt(i)-'0';
+                int add=(a+up)%10;
+                up=(a+up)/10;
+                sb=add+sb;
+            }
+        }
+        if (up!=0){
+            sb=up+sb;
+        }
+        return sb;
+    }
+}
