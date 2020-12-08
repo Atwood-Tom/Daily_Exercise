7bdd90ef13dcc43198b13c6a9d9ab9744967e0d8
Atwood-Tom <xk18407020430@163.com>
2020-12-08 23:55:47 +0800

    尼科彻斯定理.java

---
+++ NowCoder/src/尼科彻斯定理.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class 尼科彻斯定理 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNextLine()){
+            int x=sc.nextInt();
+            Solution((x)*(x-1)/2,x);
+        }
+    }
+
+    private static void Solution(int x, int time) {
+        int arr[]=new int[time];
+        for (int i=0;i<time;i++,x++){
+            if (i==time-1){
+                System.out.println((2*x+1));
+            }else {
+                System.out.print((2*x+1)+"+");
+            }
+        }
+    }
+}
---
+++ NowCoder/src/组个最小数.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class 组个最小数 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNextLine()){
+            String arr[]=sc.nextLine().split(" ");
+            int helper[]=new int[10];
+            for (int i=0;i<helper.length;i++){
+                helper[i]=Integer.parseInt(arr[i]);
+            }
+            System.out.println(Solution(helper));
+        }
+    }
+
+    private static String Solution(int[] helper) {
+        StringBuilder sb=new StringBuilder("");
+        for (int i=1;i<helper.length;i++){
+            if (helper[i]!=0){
+                helper[i]--;
+                sb.append(i);
+                break;
+            }
+        }
+        for (int i=0;i<helper.length;){
+            if (helper[i]==0){
+                i++;
+                continue;
+            }else {
+                helper[i]--;
+                sb.append(i);
+            }
+        }
+        return sb.toString();
+    }
+}
