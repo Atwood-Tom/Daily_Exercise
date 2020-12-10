5198bcf7342f03e917abc1df73ab5725b43f2c8c
Atwood-Tom <xk18407020430@163.com>
2020-12-10 22:27:25 +0800

    括号生成.java

---
+++ src/括号生成.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class 括号生成 {
+    public List<String> generateParenthesis(int n) {
+        HashMap<String,Boolean> hashMap=new HashMap<>();
+        List<String> res=new ArrayList<>();
+        int time=2*n;
+        boolean arr[]=new boolean[time];
+        String help="";
+        Solution(res,arr,help,0,time,hashMap);
+        return res;
+    }
+
+    private void Solution(List<String> res, boolean[] arr, String help, int x, int time,HashMap<String,Boolean> hash) {
+        if (x==time&&isTrue(help)&&hash.getOrDefault(help,true)){
+            res.add(help);
+            hash.put(help,false);
+            return;
+        }
+        for (int i=0;i<time;i++){
+            if (!arr[i]){
+                if (i<(time/2)){
+                    help+='(';
+                }else {
+                    help+=')';
+                }
+                arr[i]=true;
+                Solution(res,arr,help,x+1,time,hash);
+                arr[i]=false;
+                help=help.substring(0,help.length()-1);
+            }
+        }
+    }
+
+    private boolean isTrue(String help) {
+        Stack<Character> stack=new Stack<>();
+        for (int i=0;i<help.length();i++){
+            if (help.charAt(i)=='('){
+                stack.add('(');
+            }else {
+                if (stack.isEmpty()){
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        if (stack.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        括号生成 x=new 括号生成();
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        List<String> res=x.generateParenthesis(a);
+        for (String y:res){
+            System.out.println(y);
+        }
+    }
+}
