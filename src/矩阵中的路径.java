2da7006a531d68f1d977aa87fca0a7d9f018c9b8
Atwood-Tom <xk18407020430@163.com>
2020-12-19 18:32:14 +0800

    矩阵中的路径.java

---
+++ src/矩阵中的路径.java
@@ -0,0 +1,48 @@
+public class 矩阵中的路径 {
+    //链接:https://leetcode-cn.com/problems/ju-zhen-zhong-de-lu-jing-lcof/submissions/
+    public boolean exist(char[][] board, String word) {
+        char helper[][][]=new char[board.length][board[0].length][2];
+        for (int i=0;i<board.length;i++){
+            for (int j=0;j<board[0].length;j++){
+                char mid=board[i][j];
+                helper[i][j][0]=mid;
+                helper[i][j][1]='0';
+            }
+        }
+        for (int i=0;i<board.length;i++){
+            for (int j=0;j<board[0].length;j++){
+                int time=0;
+                if (Solution(helper,i,j,time,word,board.length,board[0].length)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean Solution(char[][][] board,int i, int j, int time, String word,int xlength,int ylength) {
+        if (time>=word.length()){
+            return true;
+        }else if (i<0||j<0||i>=xlength||j>=ylength){
+            return false;
+        }else if (board[i][j][1]=='*'){
+            return false;
+        }
+        if (board[i][j][0]==word.charAt(time)){
+            board[i][j][1]='*';
+           if (Solution(board,i+1,j,time+1,word,xlength,ylength)||
+                   Solution(board,i-1,j,time+1,word,xlength,ylength)||
+                   Solution(board,i,j+1,time+1,word,xlength,ylength)||
+                   Solution(board,i,j-1,time+1,word,xlength,ylength)){
+               return true;
+           }
+           board[i][j][1]='0';
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        矩阵中的路径 x=new 矩阵中的路径();
+        System.out.println(x.exist(new char[][]{{'a'}},"a"));
+    }
+}
