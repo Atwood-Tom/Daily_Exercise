1b9133fe1784079dc77d7f721afc15e3a4a5fe85
Atwood-Tom <xk18407020430@163.com>
2021-02-07 22:39:02 +0800

    非递减数列.java

---
+++ src/非递减数列.java
@@ -0,0 +1,19 @@
+public class 非递减数列 {
+    public boolean checkPossibility(int[] nums) {
+        int n = nums.length;
+        int time = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            int x = nums[i], y = nums[i + 1];
+            if (x > y) {
+                time++;
+                if (time > 1) {
+                    return false;
+                }
+                if (i > 0 && y < nums[i - 1]) {
+                    nums[i + 1] = x;
+                }
+            }
+        }
+        return true;
+    }
+}
